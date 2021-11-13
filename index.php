dbe17702ec9eda8a61e27e1c97928984d4504810
Manh Vu <vuvanmanhttvtvp@gmail.com>
2021-11-13 23:00:42 +0700

    remove content which is not use anymore

--- index.php
+++ index.php
@@ -41,50 +41,7 @@ if (isset($_SESSION['user'])) {
 
 <body class="hold-transition sidebar-mini">
     <div class="wrapper">
-        <?php
-        include "components/navbar.php";
-        include "components/sidebar.php";
-        ?>
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper pb-4">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>Người dùng</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
-                                <li class="breadcrumb-item active">Người dùng</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                </div>
-                <!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-        </div>
-        <!-- /.content-wrapper -->
-        <!-- <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.2.0-rc
-            </div>
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-        </footer> -->
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+        
     </div>
     <!-- ./wrapper -->
 
